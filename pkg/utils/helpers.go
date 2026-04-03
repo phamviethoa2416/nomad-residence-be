2d5576ffca7312aba86c885d989812ab762d7c43
phamhoa2416 <phamviethoa2416@gmail.com>
2026-04-03 10:26:48 +0700

    feat: add utility function to normalize pagination parameters

---
+++ pkg/utils/helpers.go
@@ -0,0 +1,14 @@
+package utils
+
+func NormalizePage(page, limit int) (int, int) {
+	if page <= 0 {
+		page = 1
+	}
+	if limit <= 0 {
+		limit = 20
+	}
+	if limit > 100 {
+		limit = 100
+	}
+	return page, limit
+}
