a9af754a60b2aab89b669982c71ec6bba05205f4
phamhoa2416 <phamviethoa2416@gmail.com>
2026-04-03 11:34:30 +0700

    feat: add utility functions for integer to string conversion and snake case formatting

--- pkg/utils/helpers.go
+++ pkg/utils/helpers.go
@@ -12,3 +12,42 @@ func NormalizePage(page, limit int) (int, int) {
 	}
 	return page, limit
 }
+
+func IntToStr(n int) string {
+	if n == 0 {
+		return "0"
+	}
+	negative := n < 0
+	if negative {
+		n = -n
+	}
+	digits := make([]byte, 0, 10)
+	for n > 0 {
+		digits = append([]byte{byte('0' + n%10)}, digits...)
+		n /= 10
+	}
+	if negative {
+		digits = append([]byte{'-'}, digits...)
+	}
+	return string(digits)
+}
+
+func ToSnakeCase(s string) string {
+	if s == "" {
+		return s
+	}
+
+	result := make([]byte, 0, len(s)+4)
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if c >= 'A' && c <= 'Z' {
+			if i > 0 {
+				result = append(result, '_')
+			}
+			result = append(result, c+32)
+		} else {
+			result = append(result, c)
+		}
+	}
+	return string(result)
+}
